869d30fb89a1c23f2edc49226261fddd3034db16
DariaPunda <d.punda19@gmail.com>
2023-02-28 17:01:10 +0200

    6,7 tasks

--- js/task-06.js
+++ js/task-06.js
@@ -18,3 +18,18 @@
 // #validation-input.invalid {
 //   border-color: #f44336;
 // }
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', () => {
+  
+    if (inputEl.value.length >= 6) {
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
+    } else {
+        inputEl.classList.remove('valid');
+        inputEl.classList.add('invalid');
+    }
+    console.log(inputEl.value.length);
+})
+
+
