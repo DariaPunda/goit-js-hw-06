b394f54a6ac87fa565d2a9f2d945a002c21ae798
DariaPunda <d.punda19@gmail.com>
2023-03-01 13:33:08 +0200

    edited 6 and start of 10

--- js/task-06.js
+++ js/task-06.js
@@ -22,7 +22,7 @@ const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', () => {
   
-    if (inputEl.value.length >= 6) {
+    if (inputEl.value.length === 6) {
         inputEl.classList.add('valid');
         inputEl.classList.remove('invalid');
     } else {
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,31 @@
+// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит 
+// количество элементов в input и нажимает кнопку Создать, после чего рендерится
+// коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+
+// Создай функцию createBoxes(amount), которая принимает один параметр - число. 
+// Функция создает столько < div >, сколько указано в amount и добавляет их в div#boxes.
+
+// 1. Размеры самого первого <div> - 30px на 30px.
+// 2. Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// 3. Все элементы должены иметь случайный цвет фона в формате HEX. 
+// Используй готовую функцию getRandomHexColor для получения цвета.
+
+// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes,
+// тем самым удаляя все созданные элементы.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const controlDivEl = document.querySelector('#controls');
+const divToIncrease = document.querySelector('#boxes');
+const btnCreateEl = document.querySelector('[data-create]');
+const btnDestroyEl = document.querySelector('[data-destroy]');
+
+function createBoxes(amount) {
+  const numberOfDivs = amount * document.createElement('div');
+
+  console.log(numberOfDivs);
 }
